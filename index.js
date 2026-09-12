73184d11f66b301ac2c06c94f1b181ffe5261f4d
Vishal Niwaskumar Singh <vishalsingh66193@gmail.com>
2026-09-12 19:43:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Import tickets router
 const ticketsRouter = require('./routes/tickets');
 app.use('/tickets', ticketsRouter);
+
+app.get('/', (req, res) => {
+  res.send('Employee Helpdesk API is running 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
